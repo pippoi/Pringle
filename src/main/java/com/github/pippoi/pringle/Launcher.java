6127045c71f708c8fd7ce2db662d7bef4a55fbe4
Pippoi <gh@peppo.ga>
2016-06-22 18:06:05 -0700

    Test code

---
+++ src/main/java/com/github/pippoi/pringle/Launcher.java
@@ -0,0 +1,10 @@
+package com.github.pippoi.pringle;
+
+/**
+ * Created by matt on 6/21/16.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/com/github/pippoi/pringle/LauncherTest.java
@@ -0,0 +1,14 @@
+package com.github.pippoi.pringle;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by matt on 6/21/16.
+ */
+public class LauncherTest {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+
+    }
+}
